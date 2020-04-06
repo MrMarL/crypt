4b5191ddd74e54a1da62b1c21f18e224dae5dc99
Mark <mrmarlmark@gmail.com>
2020-04-06 18:04:20 +0300

    first commit

---
+++ Crypto.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WindowsFormsApplication9
+{
+    class Crypto
+    {
+        private static readonly byte[] Salt = Encoding.ASCII.GetBytes("o6806642kbM7c5");
+
+        /// <summary>
+        /// Зашифровывает данную строку, используя AES. Строка может быть расшифрована с помощью 
+        /// DecryptStringAES().  Параметр sharedSecret должны совпадать.
+        /// </summary>
+        /// <param name="plainText">Текст для шифрования.</param>
+        /// <param name="sharedSecret">Пароль, используемый для генерации ключа для шифрования.</param>
+        public static string EncryptStringAES(string plainText, string sharedSecret)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                throw new ArgumentNullException("plainText");
+            if (string.IsNullOrEmpty(sharedSecret))
+                throw new ArgumentNullException("sharedSecret");
+
+            string outStr;                       // Зашифрованная строка для return
+            RijndaelManaged aesAlg = null;              // Объект RijndaelManaged используется для шифрования данных.
+
+            try
+            {
+                // Генерация ключ из sharedSecret и Salt.
+                var key = new Rfc2898DeriveBytes(sharedSecret, Salt);
+
+                // Создание объекта RijndaelManaged
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+
+                // Создание расшифровщика для преобразования потока.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Создание потоков, используемых для шифрования.
+                using (var msEncrypt = new MemoryStream())
+                {
+                    // подготовка IV
+                    msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
+                    msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Запись всех данных в поток.
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    outStr = Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+            finally
+            {
+                // Очистка RijndaelManaged.
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            // Возвращает зашифрованные байты из потока.
+            return outStr;
+        }
+
+        /// <summary>
+        /// Расшифровывает данную строку. Предполагается, что строка была зашифрована с использованием
+        /// EncryptStringAES(), используя идентичный sharedSecret.
+        /// </summary>
+        /// <param name="cipherText">Текст для расшифровки.</param>
+        /// <param name="sharedSecret">Пароль, используемый для генерации ключа расшифровки.</param>
+        public static string DecryptStringAES(string cipherText, string sharedSecret)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                throw new ArgumentNullException("cipherText");
+            if (string.IsNullOrEmpty(sharedSecret))
+                throw new ArgumentNullException("sharedSecret");
+
+            // Объявление объекта RijndaelManaged
+            // используемого для расшифровки данных.
+            RijndaelManaged aesAlg = null;
+
+            // Объявление строки, используемой для хранения
+            // расшифрованного текста.
+            string plaintext;
+
+            try
+            {
+                // генерирует ключ из sharedSecret и Salt
+                var key = new Rfc2898DeriveBytes(sharedSecret, Salt);
+
+                // Создание потоков, используемых для расшифровки.                
+                byte[] bytes = Convert.FromBase64String(cipherText);
+                using (var msDecrypt = new MemoryStream(bytes))
+                {
+                    // Создание объекта RijndaelManaged
+                    // с указанным ключом и IV.
+                    aesAlg = new RijndaelManaged();
+                    aesAlg.Key = key.GetBytes(aesAlg.KeySize / 8);
+                    // Получение вектора инициализации из зашифрованного потока
+                    aesAlg.IV = ReadByteArray(msDecrypt);
+                    // Создание дешифратора для выполнения потокового преобразования.
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                            // Чтение дешифрованных байтов из потока дешифрования
+                            // и помещение их в строку.
+                            plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            finally
+            {
+                // Очистка RijndaelManaged.
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+            return plaintext;
+        }
+
+        private static byte[] ReadByteArray(Stream s)
+        {
+            var rawLength = new byte[sizeof(int)];
+            if (s.Read(rawLength, 0, rawLength.Length) == rawLength.Length)
+            {
+                var buffer = new byte[BitConverter.ToInt32(rawLength, 0)];
+                if (s.Read(buffer, 0, buffer.Length) != buffer.Length)
+                {
+                    throw new SystemException("Не правильно прочитан byte массив");
+                }
+                return buffer;
+            }
+            throw new SystemException("Поток не содержит правильно отформатированный byte массив");
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form1 : Form
+    {
+        const string SharedSecret = "rath/Haste. Grace or Discipline for survivability. Grace if with a group of ES users, Discipline if solo. HatrJ*()!G*Hjkasgasikg891gjh";
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            openFileDialog2.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog2.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog3.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            Slovar.fn = openFileDialog1.FileName;
+            Slovar.File_To_List();
+            load();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Slovar.Poisk(textBox1.Text) != -1)
+                MessageBox.Show("Найдено в строке: " + Slovar.Poisk(textBox1.Text).ToString(), "Поиск", MessageBoxButtons.OK); 
+            else
+                MessageBox.Show("Не найдено.", "Не найдено", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Slovar.Poisk(textBox1.Text) != -1)
+                MessageBox.Show("Удалено в строке:" + Slovar.Ydoli(textBox1.Text).ToString(), "Del", MessageBoxButtons.OK);
+            else
+                MessageBox.Show("Не найдено.", "Не найдено", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            load();
+        }
+        public void load()
+        {
+            Slovar.list.Sort();
+            textBox2.Text = "";
+            for (int i = 0; i < Slovar.list.Count; i++)
+                if (Slovar.list[i] != "")
+                    if (i < Slovar.list.Count-1)
+                        textBox2.Text += Slovar.list[i] + Environment.NewLine;
+                    else textBox2.Text += Slovar.list[i];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog1.FileName;
+            System.IO.File.WriteAllText(filename, textBox2.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (Slovar.Poisk(textBox1.Text) == -1)
+            {
+                Slovar.Dobavka(textBox1.Text);
+                MessageBox.Show("Добавлено!", ":D", MessageBoxButtons.OK);
+            }
+            else
+                MessageBox.Show("Уже существует!", "Повтор", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            load();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (Slovar.PoiskPoSlogam(textBox1.Text) != "")
+                textBox3.Text = Slovar.PoiskPoSlogam(textBox1.Text);
+            else
+                MessageBox.Show("Не найдено.", "Не найдено", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog2.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog2.FileName;
+            System.IO.File.WriteAllText(filename, textBox3.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var encryptedStringAES = Crypto.EncryptStringAES(textBox2.Text, SharedSecret);
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog1.FileName;
+            System.IO.File.WriteAllText(filename, encryptedStringAES);
+            MessageBox.Show("Файл зашифрован");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.Cancel)
+                return;
+            string netxt = System.IO.File.ReadAllText(openFileDialog2.FileName);
+            textBox2.Text = Crypto.DecryptStringAES(netxt, SharedSecret);
+            Slovar.list = new List<string>(textBox2.Lines);
+            load();
+        }
+    }
+
+    class Slovar
+    {
+        static public string fn = "Словарь.txt";
+        static public List<string> list = new List<string>();
+
+        static public bool File_To_List()     // загрузить файл в список
+        {
+            list.Clear();
+            try
+            {
+                System.IO.StreamReader f = new System.IO.StreamReader(fn);
+                while (!f.EndOfStream)
+                {
+                    list.Add(f.ReadLine());
+                }
+                f.Close();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка доступа к файлу!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        static public int Ydoli(string slovo)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == slovo && slovo != "")
+                {
+                    list[i] = "";
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static public void Dobavka(string slovo)
+        {
+            list.Add(slovo);
+        }
+
+        static public int Poisk(string slovo)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == slovo && slovo != "")
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static public string PoiskPoSlogam(string slovo)
+        {
+            string otv = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                for(int s = 0,q=0; s < slovo.Length; s++){
+                    if ((list[i].Length >=slovo.Length) && ((list[i])[s] == slovo[s]) && (slovo != ""))
+                    {
+                        ++q;
+                        if (q == slovo.Length)
+                            otv += list[i] + Environment.NewLine;
+                    }
+                }
+            }
+            return otv;
+        }
+    }
+}
